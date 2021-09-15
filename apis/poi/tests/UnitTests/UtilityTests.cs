af0f9aa835b07bc1be25c3549045f72075f58a7b
Liping Huang <73298493+lipinght@users.noreply.github.com>
2021-09-15 12:59:13 +0100

    fail unit test 5

--- apis/poi/tests/UnitTests/UtilityTests.cs
+++ apis/poi/tests/UnitTests/UtilityTests.cs
@@ -8,7 +8,7 @@ namespace UnitTests
         [Fact]
         public void TestLoggingEvents()
         {
-            Assert.Equal(1, LoggingEvents.Healthcheck);
+            Assert.Equal(2, LoggingEvents.Healthcheck);
             Assert.Equal(2001, LoggingEvents.GetAllPOIs);
             Assert.Equal(2002, LoggingEvents.GetPOIByID);
             Assert.Equal(2002, LoggingEvents.GetPOIByTripID);
