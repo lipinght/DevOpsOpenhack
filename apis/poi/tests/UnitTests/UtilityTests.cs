87aca7a00e687702fade805767f855f4842ea077
Liping Huang <73298493+lipinght@users.noreply.github.com>
2021-09-15 12:43:39 +0100

    Update UtilityTests.cs

--- apis/poi/tests/UnitTests/UtilityTests.cs
+++ apis/poi/tests/UnitTests/UtilityTests.cs
@@ -8,7 +8,7 @@ namespace UnitTests
         [Fact]
         public void TestLoggingEvents()
         {
-            Assert.Equal(1000, LoggingEvents.Healthcheck-1);
+            Assert.Equal(1000, LoggingEvents.Healthcheck-2);
             Assert.Equal(2001, LoggingEvents.GetAllPOIs);
             Assert.Equal(2002, LoggingEvents.GetPOIByID);
             Assert.Equal(2002, LoggingEvents.GetPOIByTripID);
