e123a521de829432543cd718762be2e00be45619
Paul Verboom <verboompj@outlook.com>
2021-09-16 14:49:53 +0200

    Create polling.ps1

---
+++ polling.ps1
@@ -0,0 +1,18 @@
+Param(
+    [string] [Parameter(Mandatory=$true)] $Uri,
+    [boolean] [Parameter(Mandatory=$false)] $displayUri
+    )
+
+while($true) {
+  $R = Invoke-WebRequest -URI $Uri
+  $timestamp = Get-Date
+  $output = ""
+  if ($displayUri) {
+    $output = $output = '{0} | {1} | {2}' -f($timestamp, $R.StatusCode, $Uri)
+  } else {
+
+    $output = '{0} | {1}' -f($timestamp, $R.StatusCode) 
+  }
+  Write-Output $output
+  Start-Sleep -Seconds 1
+}
